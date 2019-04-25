2a1d5bf9a7d32e9e5aa64dc9509dcf9c0d9b1dd7
ericmadureira <ericmadureira.uesc@gmail.com>
2019-04-24 23:57:25 -0300

    add initial page assets

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import { Image, StyleSheet, Text, View, ImageBackground } from 'react-native';
+// import { Button, Card } from 'react-native-paper';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <ImageBackground source={require('./assets/dead-lift.jpg')} style={{width: '100%', height: '100%', padding: 0, margin: 0}}>
+          <Text style={styles.title}>ProFitness</Text>
+          <Text style={styles.paragraph}>Not a member?</Text>
+          <Text style={styles.paragraph}>A ProFusion product ®2019</Text>
+        </ImageBackground>
       </View>
     );
   }
@@ -17,5 +22,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
 });
